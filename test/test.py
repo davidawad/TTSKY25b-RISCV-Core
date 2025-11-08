57286e70fcb640a7632989095f6485f925eeb6ca
david awad <me@davidaw.ad>
2025-11-07 16:01:16 -0800

    Update test functions for clock period and assertions

--- test/test.py
+++ test/test.py
@@ -27,45 +27,11 @@ async def uart_decoder(rx, period):
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
-
-
-@cocotb.test()
 async def test_uart_functionality(dut):
     dut._log.info("Start UART test")
 
     # Set the clock period to 50 ns (20 MHz)
-    clock = Clock(dut.clk, 50, units="ns")
+    clock = Clock(dut.clk, 50, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
